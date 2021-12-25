6a96fd6fed0f3366846d1aa329689064cf6ae3c6
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 11:01:55 +0300

    Scripted auto-commit on change (2021-12-25 11:01:55) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab4/TestPackage.java
+++ src/main/java/ru/bmstu/akka/lab4/TestPackage.java
@@ -1,7 +1,8 @@
 package ru.bmstu.akka.lab4;
 
 public class TestPackage {
-    final private String PACKAGE_ID = "packageId";
-    final private String JS_SCRIPT = "jsScript";
-    final []
+    final static private String PACKAGE_ID = "packageId";
+    final static private String JS_SCRIPT = "jsScript";
+    final static private String TESTS = "tests";
+
 }
