14e09d4daedbfe14d44ba72590ad8b14a9b6b3f5
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 10:56:08 +0300

    Scripted auto-commit on change (2021-12-25 10:56:08) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab4/HttpServer.java
+++ src/main/java/ru/bmstu/akka/lab4/HttpServer.java
@@ -3,6 +3,7 @@ package ru.bmstu.akka.lab4;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.server.Route;
 
 public class HttpServer {
     ActorSystem system;
@@ -10,5 +11,8 @@ public class HttpServer {
     public HttpServer(ActorSystem system) {
         this.system = system;
 //        system.actorOf(Props.create())
+    }
+    public Route getRoute() {
+        return 
     }
 }
