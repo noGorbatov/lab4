b109ca9b68a6b0e23f3e69a85970de10218fdee4
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 10:52:14 +0300

    Scripted auto-commit on change (2021-12-25 10:52:14) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab4/StorageActor.java
+++ src/main/java/ru/bmstu/akka/lab4/StorageActor.java
@@ -3,19 +3,29 @@ package ru.bmstu.akka.lab4;
 import akka.actor.AbstractActor;
 
 public class StorageActor extends AbstractActor {
-
+    Map<Integer, String> storage =
     public static class StoreMsg {
-        private int packageId;
-        private String results;
+        private final int packageId;
+        private final String results;
         public StoreMsg(int id, String results) {
             this.packageId = id;
             this.results = results;
+        }
+        public int getPackageId() {
+            return packageId;
+        }
+        public String getResults() {
+            return results;
         }
     }
 
     @Override
     public Receive createReceive() {
         return receiveBuilder().
-                match()
+                match(StoreMsg.class, )
+    }
+
+    private void store(StoreMsg msg) {
+
     }
 }
