54ef81cbfde78baba1340be9dfa6e123284f363a
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 10:57:21 +0300

    Scripted auto-commit on change (2021-12-25 10:57:21) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab4/HttpServer.java
+++ src/main/java/ru/bmstu/akka/lab4/HttpServer.java
@@ -3,10 +3,10 @@ package ru.bmstu.akka.lab4;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Directives.*;
 import akka.http.javadsl.server.Route;
 
-public class HttpServer extends AllDirectives {
+public class HttpServer {
     ActorSystem system;
     ActorRef storageActor;
     public HttpServer(ActorSystem system) {
