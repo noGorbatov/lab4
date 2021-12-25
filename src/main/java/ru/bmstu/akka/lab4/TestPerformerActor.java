fb6f4e327d3910a5eb71cfa0bfa2dbf459068a59
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 11:06:49 +0300

    Scripted auto-commit on change (2021-12-25 11:06:49) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab4/TestPerformerActor.java
+++ src/main/java/ru/bmstu/akka/lab4/TestPerformerActor.java
@@ -3,5 +3,9 @@ package ru.bmstu.akka.lab4;
 import akka.actor.AbstractActor;
 
 public class TestPerformerActor extends AbstractActor {
-    
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
