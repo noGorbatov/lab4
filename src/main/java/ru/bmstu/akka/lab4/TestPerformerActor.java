a46fbcf018873558449709e24b5232caeacefc41
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 11:07:01 +0300

    Scripted auto-commit on change (2021-12-25 11:07:01) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab4/TestPerformerActor.java
+++ src/main/java/ru/bmstu/akka/lab4/TestPerformerActor.java
@@ -1,11 +1,32 @@
 package ru.bmstu.akka.lab4;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 
 public class TestPerformerActor extends AbstractActor {
 
+    public static class RunTestsMsg {
+        final private TestPackage testPackage;
+        final private ActorRef storageActor;
+        public RunTestsMsg(TestPackage tests, ActorRef storageActor) {
+            this.testPackage = tests;
+            this.storageActor = storageActor;
+        }
+        public ActorRef getStorageActor() {
+            return storageActor;
+        }
+        public TestPackage getTestPackage() {
+            return testPackage;
+        }
+    }
+
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder().
+                match(RunTestsMsg.class, );
+    }
+
+    private void runTests(RunTestsMsg tests) {
+        
     }
 }
