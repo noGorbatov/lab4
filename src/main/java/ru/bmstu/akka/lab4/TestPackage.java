d56ab0cea318b9a17b6368bbd4a633a74dec17ee
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 11:07:42 +0300

    Scripted auto-commit on change (2021-12-25 11:07:42) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab4/TestPackage.java
+++ src/main/java/ru/bmstu/akka/lab4/TestPackage.java
@@ -9,6 +9,7 @@ public class TestPackage {
     final static private String PACKAGE_ID = "packageId";
     final static private String JS_SCRIPT = "jsScript";
     final static private String TESTS = "tests";
+    final static private String FUNC_NAME = "functionName";
 
     final private int id;
     final private String script;
@@ -18,9 +19,11 @@ public class TestPackage {
     @JsonCreator
     public TestPackage(@JsonProperty(PACKAGE_ID) int id,
                        @JsonProperty(JS_SCRIPT) String script,
+                       @JsonProperty(FUNC_NAME) String functionName,
                        @JsonProperty(TESTS) List<TestData> tests) {
         this.id = id;
         this.script = script;
+        this.functionName = functionName;
         this.tests = tests;
     }
 
@@ -28,11 +31,15 @@ public class TestPackage {
         return id;
     }
 
-    public static String getJsScript() {
-        return JS_SCRIPT;
+    public String getJsScript() {
+        return script;
     }
 
     public List<TestData> getTests() {
         return tests;
+    }
+
+    public String getFuncName() {
+        return functionName;
     }
 }
