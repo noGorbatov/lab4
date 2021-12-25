5d941131f067e0c931acba361a05d6f51f762dcf
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 11:02:12 +0300

    Scripted auto-commit on change (2021-12-25 11:02:12) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab4/TestData.java
+++ src/main/java/ru/bmstu/akka/lab4/TestData.java
@@ -1,2 +1,11 @@
-package ru.bmstu.akka.lab4;public class TestData {
+package ru.bmstu.akka.lab4;
+
+public class TestData {
+    final static private String TEST_NAME = "testName";
+    final static private String EXPECTED = "expectedResult";
+    final static private String PARAMETERS = "params";
+
+    final private String testName;
+    final private String expected;
+    final private 
 }
--- src/main/java/ru/bmstu/akka/lab4/TestPackage.java
+++ src/main/java/ru/bmstu/akka/lab4/TestPackage.java
@@ -1,8 +1,14 @@
 package ru.bmstu.akka.lab4;
 
+import java.util.List;
+
 public class TestPackage {
     final static private String PACKAGE_ID = "packageId";
     final static private String JS_SCRIPT = "jsScript";
     final static private String TESTS = "tests";
 
+    private int id;
+    private String script;
+    private String functionName;
+    private List<TestData> tests;
 }
