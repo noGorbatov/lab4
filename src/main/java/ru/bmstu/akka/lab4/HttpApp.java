2249d4ba15c62d5097a4320e7adbf1a1fe76734f
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 11:13:35 +0300

    Scripted auto-commit on change (2021-12-25 11:13:35) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab4/HttpApp.java
+++ src/main/java/ru/bmstu/akka/lab4/HttpApp.java
@@ -1,14 +1,35 @@
 package ru.bmstu.akka.lab4;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 public class HttpApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("HttpSystem");
         Http http = Http.get(system);
+        ActorMaterializer materializer = ActorMaterializer.create(system);
         HttpServer server = new HttpServer(system);
-        
+        Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                server.getRoute().flow(system, materializer);
+
+        CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+
+        System.in.read();
+        binding.
+                thenCompose(ServerBinding)
 
     }
 }
