5698447dcf67d4376ab8d3ae21d35518ef10dcbf
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 11:13:32 +0300

    Scripted auto-commit on change (2021-12-25 11:13:32) by gitwatch.sh

--- src/main/java/ru/bmstu/akka/lab4/HttpApp.java
+++ src/main/java/ru/bmstu/akka/lab4/HttpApp.java
@@ -7,6 +7,8 @@ public class HttpApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("HttpSystem");
         Http http = Http.get(system);
+        HttpServer server = new HttpServer(system);
+        
 
     }
 }
